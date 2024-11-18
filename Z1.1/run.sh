6154a02a1ed326ec387607bce946e03f8fdf2106
ksokol <01178595@pw.edu.pl>
2024-11-18 09:38:52 +0100

    minor fix in run sh

--- Z1.1/run.sh
+++ Z1.1/run.sh
@@ -18,7 +18,7 @@ docker logs --since=1h ${client_id} > ./z32_results/client_c.txt
 
 echo python to python demonstration, results in server_py.txt and client_py.txt
 
-server_id="$(docker run -it -d --network z32_network --hostname z32_server --network-alias z32_server_py --name z32_server_py z32_server_py --IP z32_server_py)"
+server_id="$(docker run -it -d --network z32_network --hostname z32_server_py --network-alias z32_server_py --name z32_server_py z32_server_py --IP z32_server_py)"
 client_id="$(docker run -it -d --network z32_network --hostname z32_client_py --network-alias z32_client_py --name z32_client_py z32_client_py --IP z32_server_py)"
 
 
@@ -47,7 +47,7 @@ sleep 10
 docker rm z32_client_py
 docker rm z32_server
 
-server_id="$(docker run -it -d --network z32_network --hostname z32_server --network-alias z32_server_py --ip '172.21.32.2' --name z32_server_py z32_server_py --IP z32_server_py --PORT 8080)"
+server_id="$(docker run -it -d --network z32_network --hostname z32_server_py --network-alias z32_server_py --ip '172.21.32.2' --name z32_server_py z32_server_py --IP z32_server_py --PORT 8080)"
 client_id="$(docker run -it -d --network-alias z32_client --ip '172.21.32.3' --network z32_network --name z32_client z32_client)"
 
 docker logs --since=1h ${server_id} > ./z32_results/cross_server_py.txt
