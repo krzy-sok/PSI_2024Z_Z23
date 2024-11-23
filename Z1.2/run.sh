bdf62abf764cf3a638332b39327a6cbb7d15c044
ksokol <01178595@pw.edu.pl>
2024-11-23 15:31:34 +0100

    typo

--- Z1.2/run.sh
+++ Z1.2/run.sh
@@ -2,6 +2,6 @@
 
 docker compose run
 docker compose up > result1_2.txt &
-sleep 11
+sleep 1
 #docker ps | grep "z32"
 docker exec z32_client1_2 tc qdisc add dev eth0 root netem delay 1000ms 500ms loss 33%
