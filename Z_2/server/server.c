0d2da46e95af24c2c0a5156ebff708eff60a542e
ksokol <01178595@pw.edu.pl>
2024-12-01 18:39:02 +0100

    increased server buffer size

--- Z_2/server/server.c
+++ Z_2/server/server.c
@@ -10,7 +10,7 @@ int main() {
     int server_fd, new_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
-    char buffer[1024];  
+    char buffer[10240];
 
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
@@ -19,27 +19,27 @@ int main() {
     }
 
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;  
+    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
         return -1;
     }
 
-   
+
     if (listen(server_fd, 3) < 0) {
         return -1;
     }
-    
+
     printf("Serwer oczekuje na połączenie...\n");
 
-   
+
     if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0) {
         return -1;
     }
     printf("Połączenie nawiązane.\n");
 
-    
+
     while (1) {
         int bytes_read = read(new_socket, buffer, sizeof(buffer));
         if (bytes_read <= 0) {
@@ -47,7 +47,7 @@ int main() {
             break;
         }
         printf("Odebrano %d bajtów.\n", bytes_read);
-        sleep(1);  
+        sleep(1);
     }
 
     close(new_socket);
